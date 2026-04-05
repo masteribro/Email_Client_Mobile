104cad08517c29a55e6eb12d0bdd9fa3df427ca2
masteribro <mohammedibrahim075@gmail.com>
2026-04-05 09:06:40 +0100

    move router to its own class

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,38 @@
+import 'package:go_router/go_router.dart';
+
+import '../../presentation/screens/compose/compose_screen.dart';
+import '../../presentation/screens/email_detail/email_detail_screen.dart';
+import '../../presentation/screens/inbox/inbox_screen.dart';
+import '../../presentation/screens/login/login_screen.dart';
+
+class AppRouter {
+  static final router = GoRouter(
+    initialLocation: '/login',
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/inbox',
+        builder: (context, state) => const InboxScreen(),
+      ),
+      GoRoute(
+        path: '/email/:id',
+        builder: (context, state) =>
+            EmailDetailScreen(emailId: state.pathParameters['id']!),
+      ),
+      GoRoute(
+        path: '/compose',
+        builder: (context, state) {
+          final extra = state.extra as Map<String, String>?;
+          return ComposeScreen(
+            replyToEmail: extra?['replyToEmail'],
+            replyToName: extra?['replyToName'],
+            replySubject: extra?['replySubject'],
+          );
+        },
+      ),
+    ],
+  );
+}
